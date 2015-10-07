9b58cc91c2bf4ee75394ea27a0a279de8062a60b
lcanet <lc@lc6.net>
2015-10-07 23:14:25 +0200

    More ES6

--- src/Map.js
+++ src/Map.js
@@ -1,21 +1,28 @@
 import { leaf } from 'leaflet';
 
-export class Map {
-    constructor() {
-        this.restrict = 'E';
+class BaseDirective {
+    constructor(restrict, template) {
+        this.restrict = restrict;
         this.replace = true;
-        // multilines
-        this.template = `<div style="width: 300px; height:300px">
-        </div>`
+        this.template = template;
+    }
+}
+
+// inheritance
+export class Map extends BaseDirective {
+    constructor() {
+        // multiline string
+        super('E',
+        `<div style="width: 400px; height:300px">
+        </div>`)
     }
 
-    // arrow functions
     link (scope, elt, attrs) {
 
         let map = L.map(elt[0]).setView([47, 4], 5);
 
         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors and friends'
         }).addTo(map);
     }
 }
